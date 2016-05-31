57682b738ae2d20973282f1435641c1486d76ace
Eray Alakese <erayalakese@gmail.com>
2016-05-31 03:33:20 +0300

    request dependency removed, now this the module has ZERO dependency

--- index.js
+++ index.js
@@ -1,21 +1,29 @@
-var request = require('request');
+var https = require('https');
 
 module.exports = {
 	get : function(username, cb, err) {
 		var options = {
-			url: 'https://api.github.com/users/'+ username +'/repos',
+      host: 'api.github.com',
+      path: '/users/'+ username +'/repos',
+      port: 443,
 			headers: {
 				'User-Agent': 'node-github-repos'
 			}
 		};
 
-		request(options, function (error, response, body) {
-		  if (!error && response.statusCode == 200) {
-		    cb(body);
-		  }
-		  else {
-		  	err(response.statusCode);
-		  }
+		var req = https.request(options, function (response) {
+      var res = '';
+      response.on('data', function(data) {
+        res += data.toString();
+      });
+      response.on('end', function() {
+        cb(JSON.parse(res));
+      });
+      req.on('error', function(err) {
+        console.log(err);
+        err(err.statusCode);
+      });
 		});
+    req.end();
 	}
 };
